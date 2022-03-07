6c0c2b203a1a958b2094bd213f946daf5a61d671
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-07 14:48:46 +0900

    gitignore file added

--- SRC/App.js
+++ SRC/App.js
@@ -1,66 +1,62 @@
 const Card = (props) => {
-  return (
-    React.createElement("div", {}, [
-      React.createElement("img", {}, props.frontfase),
-      React.createElement("img", {}, props.backfase),
-    ]));
-}
+  return React.createElement("div", {}, [
+    React.createElement("img", {}, props.frontfase),
+    React.createElement("img", {}, props.backfase),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-        React.createElement("h1", {}, "Memory Game!"),
-        React.createElement(Card, {
-          frontface: "Chase",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Chase",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Everest",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Everest",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Marshal",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Marshal",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Rocky",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Rocky",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Rubble",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Rubble",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Skye",
-          backface: "PAW_Patrol"
-        }),
-        React.createElement(Card, {
-          frontface: "Skye",
-          backface: "PAW_Patrol"
-        })
-      ]);
-  };
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Memory Game!"),
+    React.createElement(Card, {
+      frontface: "Chase",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Chase",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Everest",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Everest",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Marshal",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Marshal",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Rocky",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Rocky",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Rubble",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Rubble",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Skye",
+      backface: "PAW_Patrol",
+    }),
+    React.createElement(Card, {
+      frontface: "Skye",
+      backface: "PAW_Patrol",
+    }),
+  ]);
+};
 
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
