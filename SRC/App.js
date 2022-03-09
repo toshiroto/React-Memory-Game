44b7bb3590faba9d400767e41286d0dbfbe3f241
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-09 15:07:45 +0900

    refactored App function

--- SRC/App.js
+++ SRC/App.js
@@ -1,27 +1,21 @@
-
 import { render } from "react-dom";
 import characters from "./characters";
 import Paw from "./Paw";
-
-const createCard = (character) => {
-  return (
-  <Paw
-    id={character.id}
-    key={character.id}
-    name={character.name}
-    img={character.imgUrl}
-  />
-  );
-};
 
 const App = () => {
   return (
     <div className="memory-game">
       <h1>Paw Patrol Memory Game</h1>
-      {characters.map(createCard)}
-
+      {characters.map((character) => (
+        <Paw
+          id={character.id}
+          key={character.id}
+          name={character.name}
+          img={character.imgUrl}
+        />
+      ))}
     </div>
   );
-}
+};
 
- render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
